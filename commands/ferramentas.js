832975afa0784dafef6bcacad9887c917d8c1d6a
username20invalid <165141361+username20invalid@users.noreply.github.com>
2026-04-28 11:41:37 -0700

    Update ferramentas.js

--- commands/ferramentas.js
+++ commands/ferramentas.js
@@ -4,11 +4,11 @@ module.exports = {
     name: 'ping',
     command: ['ping', 'latencia', 'test'],
     tags: 'ferramentas',
-    desc: 'Testa a latência do bot',
+    desc: 'Testa latência',
     run: async (sock, { from }) => {
         const start = Date.now();
-        await sock.sendMessage(from, { text: '🏓 *Pong!*' });
+        await sock.sendMessage(from, { text: '🏓 Pong!' });
         const end = Date.now();
-        await sock.sendMessage(from, { text: `⚡ *Latência:* ${end - start}ms` });
+        await sock.sendMessage(from, { text: `⚡ Latência: ${end - start}ms` });
     }
 };
