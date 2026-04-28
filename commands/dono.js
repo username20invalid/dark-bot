af805aabde3e7fd5456fc71d3c7949f5c0563da5
username20invalid <165141361+username20invalid@users.noreply.github.com>
2026-04-28 11:40:53 -0700

    Update dono.js

--- commands/dono.js
+++ commands/dono.js
@@ -2,23 +2,10 @@ module.exports = {
     name: 'dono',
     command: ['dono', 'owner', 'criador'],
     tags: 'info',
-    desc: 'Mostra informações do dono',
+    desc: 'Info do dono',
     run: async (sock, { from, isOwner }) => {
-        const vcard = 'BEGIN:VCARD\n' +
-            'VERSION:3.0\n' +
-            'FN:👑 Dono DARK BOT\n' +
-            'TEL;type=CELL;type=VOICE;waid=244949926074:+244949926074\n' +
-            'END:VCARD';
-
-        await sock.sendMessage(from, { 
-            contacts: { 
-                displayName: '👑 Dono DARK BOT',
-                contacts: [{ vcard }] 
-            } 
-        });
-        
-        if (isOwner) {
-            await sock.sendMessage(from, { text: '👑 *Você é o dono do bot!*' });
-        }
+        const vcard = 'BEGIN:VCARD\nVERSION:3.0\nFN:👑 Dono DARK BOT\nTEL;type=CELL;type=VOICE;waid=244949926074:+244949926074\nEND:VCARD';
+        await sock.sendMessage(from, { contacts: { displayName: '👑 Dono DARK BOT', contacts: [{ vcard }] } });
+        if (isOwner) await sock.sendMessage(from, { text: '👑 *Você é o dono do bot!*' });
     }
 };
