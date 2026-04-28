a9bdfb856c633c85e214652887a79161e895c9c1
username20invalid <165141361+username20invalid@users.noreply.github.com>
2026-04-28 11:39:36 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,8 @@
 module.exports = {
-    // NÚMERO DO DONO (com código do país, sem +)
     ownerNumber: '244949926074',
-    
-    // NOME DO BOT
     botName: 'DARK BOT',
-    
-    // VERSÃO
     version: 'v1.0',
-    
-    // PREFIXO DOS COMANDOS
     prefix: '!',
-    
-    // RECUSAR CHAMADAS?
     rejectCalls: true,
-    
-    // NOME DO DONO
-    ownerName: '👑 Dark'
+    ownerName: 'Dark'
 };
